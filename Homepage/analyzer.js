f999b46cdf6c0370fc5da0307c9edce661d02639
Alex <ataffe@asu.edu>
2017-07-17 15:42:15 -0700

    added function for testing print.

--- Homepage/analyzer.js
+++ Homepage/analyzer.js
@@ -1,10 +1,14 @@
 /**
  * Created by Alexxx on 7/12/2017.
  */
-function Analyzer() {
-    var numWords;
-    var mainText;
-    var words[];
-    var topWords[];
+var Analyzer = {
+    var numWords = 0;
+    var mainText = "test";
+    var words = new Array("");
+    var topWords = new Array("");
 
+    print = function(text){
+        this.mainText = mainText + text;
+        document.getElementById("resBox").value += this.mainText;
+    }
 }
