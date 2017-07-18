d4533d434d5f88f1eca9f9bf5cea648c9ec45a2e
Alex <ataffe@asu.edu>
2017-07-18 06:57:26 -0700

    added wordCount function

--- Homepage/analyzer.js
+++ Homepage/analyzer.js
@@ -1,10 +1,45 @@
 /**
  * Created by Alexxx on 7/12/2017.
  */
-function Analyzer() {
-    var numWords;
-    var mainText;
-    var words[];
-    var topWords[];
+var Analyzer() = {
+    numWords : 0;
+    var mainText = "";
+    var words = new Array("none");
+    var topWords = new Array("none");
+
+    readText = function (text) {
+        this.mainText = text;
+    };
+
+    getWords = function () {
+        return words;
+    }
+
+    toString = function () {
+        var returnString = "Text statistics\n\n"
+         + "----------------------------------------------"
+         + "----------------------------------------------\n\n"
+         + "Number of Words: " + numWords + "\n";
+        document.getElementById("")
+    }
+
+    countWords = function () {
+        var num_words = 0;
+        var regex = /\n|\s{2,}/g;
+        var words = mainText.split(regex)
+        var list = [];
+
+        for(var i = 0; i < words.length; i++){
+            var letters = words[i].split("");
+
+            if(letters.length != 0){
+                list.push(words[i]);
+            }
+        }
+
+        words = letters.join();
+        num_words = words.length;
+        return num_words;
+    }
 
 }
