1d0ae9a03166ff989b0afe768b83478660a9dc06
Alex <ataffe@asu.edu>
2017-07-20 07:11:48 -0700

    Moved javascript to html.

--- Homepage/analyzer.js
+++ Homepage/analyzer.js
@@ -1,57 +1,54 @@
 /**
  * Created by Alexxx on 7/12/2017.
- */
-<<<<<<< HEAD
-var Analyzer() = {
-    numWords : 0;
-    var mainText = "";
-    var words = new Array("none");
-    var topWords = new Array("none");
+ ***/
 
-    readText = function (text) {
-        this.mainText = text;
-    };
+<script>
 
-    getWords = function () {
-        return words;
-    }
+    var Analyzer() = {
+        var numWords = 0;
+        var mainText = "This is a test";
+        var words = new Array("none");
+        var topWords = new Array("none");
 
-    toString = function () {
-        var returnString = "Text statistics\n\n"
-         + "----------------------------------------------"
-         + "----------------------------------------------\n\n"
-         + "Number of Words: " + numWords + "\n";
-        document.getElementById("")
-    }
+        readText = function (text) {
+            this.mainText = text;
+        };
 
-    countWords = function () {
-        var num_words = 0;
-        var regex = /\n|\s{2,}/g;
-        var words = mainText.split(regex)
-        var list = [];
-
-        for(var i = 0; i < words.length; i++){
-            var letters = words[i].split("");
-
-            if(letters.length != 0){
-                list.push(words[i]);
-            }
+        getWords = function () {
+            return words;
         }
 
-        words = letters.join();
-        num_words = words.length;
-        return num_words;
-    }
-=======
-var Analyzer = {
-    var numWords = 0;
-    var mainText = "test";
-    var words = new Array("");
-    var topWords = new Array("");
->>>>>>> origin/master
+        toString = function () {
+            var returnString = "Text statistics\n\n"
+             + "----------------------------------------------"
+             + "----------------------------------------------\n\n"
+             + "Number of Words: " + numWords + "\n";
+            document.getElementById("")
+        }
 
-    print = function(text){
-        this.mainText = mainText + text;
-        document.getElementById("resBox").value += this.mainText;
+        countWords = function () {
+            var num_words = 0;
+            var regex = /\n|\s{2,}/g;
+            var words = mainText.split(regex)
+            var list = [];
+
+            for(var i = 0; i < words.length; i++){
+                var letters = words[i].split("");
+
+                if(letters.length != 0){
+                    list.push(words[i]);
+                }
+            }
+
+            words = letters.join();
+            num_words = words.length;
+            return num_words;
+        }
     }
-}
+
+    test = function() {
+        mainText = mainText + "Alex";
+        document.getElementById("resBox").value += mainText.textContent || mainText.innerText;
+    }
+
+</script>
