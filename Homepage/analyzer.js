ab04a102d016c05214209f318224901e1b755cae
Alex <ataffe@asu.edu>
2017-09-13 00:20:52 -0700

    added word frequency and additional checks for other feature so they are only called when the text in the box changes.

--- Homepage/analyzer.js
+++ Homepage/analyzer.js
@@ -32,7 +32,9 @@ var longestWordString = "";
 var trieMade = false;
 var savedText = "";
 var definition = "";
-
+var letterQ = [];
+var topWords = [];
+var mostUsedWords = "";
 /**
  * this function reads the text from input box and stores it
  * @param text
@@ -46,6 +48,7 @@ readText = function (text) {
  *
  */
 function getDefinition(word){
+    longestWordString = "";
     var xhttp = new XMLHttpRequest();
     var url = "http://api.pearson.com/v2/dictionaries/lasde/entries?headword=" + word.toLowerCase();
     console.log(url);
@@ -55,7 +58,11 @@ function getDefinition(word){
             var part_of_speech = definitionRaw.results[0].part_of_speech;
             definition = definitionRaw.results[0].senses[0].definition[0];
             longestWordString += word.charAt(0).toUpperCase() + word.slice(1) + " (" + part_of_speech + ") - " + definition + "<br/><br/>";
-            document.getElementById("resTable").rows[2].cells[1].innerHTML = longestWordString;
+            if(longestWordString.length > 0) {
+                document.getElementById("resTable").rows[2].cells[1].innerHTML = longestWordString;
+            }else{
+                document.getElementById("resTable").rows[2].cells[1].innerHTML = word;
+            }
 
         }
     };
@@ -153,7 +160,6 @@ function search(vertex, word, add) {
         if(add === true) {
             vertex.frequency++;
         }
-
         return true;
     }else{
         return false;
@@ -166,7 +172,7 @@ function search(vertex, word, add) {
  * @param word The word to be cleaned.
  * @returns {*} the cleaned word.
  */
-function cleanWord (word) {
+function CleanWord (word) {
     for(var i = 0 ; i < word.length; i++){
         var char = word.charCodeAt(i);
         if(char < 65 || char > 122 || (char > 90 && char < 97)){
@@ -179,7 +185,68 @@ function cleanWord (word) {
     return word;
 }
 
-//The root of the trie.
+/**
+ * @return {string}
+ */
+function GetWord (){
+    var retString = "";
+    while(letterQ.length > 0){
+        retString += letterQ.shift();
+    }
+    return retString;
+}
+
+function InsertWord(word, frequency) {
+    var wordFreq = [word, frequency];
+    if(frequency > topWords[0] || topWords.length < 10){
+        var inserted = false;
+        if(topWords.length < 10) {
+            topWords.push(wordFreq);
+        }else {
+            for (var i = 1; i < 10 && !inserted; i++) {
+                if (frequency < topWords[i]) {
+                    topWords[i] = wordFreq;
+                    inserted = true;
+                }
+            }
+        }
+
+        topWords.sort(function (a, b){return a - b;});
+    }
+}
+
+function getWordFrequency(root) {
+    for(var i = 0; i < 26; i++){
+        if(root.children[i] !== null){
+            if(root.children[i].isLeaf){
+                letterQ.push(String.fromCharCode(97 + i));
+                var word = GetWord();
+                InsertWord(word,root.children[i].frequency);
+            }else{
+                letterQ.push(String.fromCharCode(97 + i));
+                getWordFrequency(root.children[i]);
+            }
+        }
+    }
+}
+
+/**
+ * This function checks a given array for the given word.
+ * @param word the word that is going to be looked for
+ * @param array the array that will be looked through
+ * @returns {boolean} is the word found or not
+ */
+function containsWord(word, array) {
+    var found = false;
+    for(var i = 0; i < array.length && !found; i++){
+        if(array[i].toLowerCase() === word.toLowerCase()){
+            found = true;
+        }
+    }
+    return found;
+}
+
+//I needed to crate a node for the Trie
 root = new Node();
 
 /**
@@ -206,7 +273,7 @@ function readWords() {
 
     if(tmpWords !== null) {
         for (var i = 0; i < tmpWords.length; i++) {
-            var word = cleanWord(tmpWords[i]);
+            var word = CleanWord(tmpWords[i]);
 
             //Find the longest word
             if (i === 0) {
@@ -215,7 +282,7 @@ function readWords() {
                 if (word.length > longestWords[0].length) {
                     longestWords = [];
                     longestWords.push(word);
-                } else if (word.length === longestWords[0].length) {
+                } else if (word.length === longestWords[0].length && !containsWord(word,longestWords)) {
                     longestWords.push(word);
                 }
             }
@@ -236,15 +303,30 @@ function readWords() {
                     }
                 }
         }
+
+        //get longest word definition for RESTful api call
+        if(!trieMade) {
+            console.log("longest words size: " + longestWords.length);
+            //This get all the longest words if there are multiple words
+            for (var j = 0; j < longestWords.length; j++) {
+                getDefinition(longestWords[j]);
+                console.log("longest word: " + longestWords);
+            }
+        }
+
+        //get word frequency from trie
+        if(!trieMade) {
+            topWords = [];
+            getWordFrequency(this.root);
+            for (var k = 0; k < topWords.length; k++) {
+                mostUsedWords += topWords[k][0] + ": " + topWords[k][1] + "<br/>";
+            }
+        }
+
         if (numWords > 0) {
             wordsRead = true;
             trieMade = true;
             savedText = mainText;
-        }
-
-        //This get all the longest words if there are multiple words
-        for (var j = 0; j < longestWords.length; j++) {
-            getDefinition(longestWords[j]);
         }
 
     }
